cc0f9f3544d37731990a78c3d527ca7725d3f797
maxrt <max.r.tkachuk@gmail.com>
2025-10-13 03:46:59 +0300

    Fix multiple command in one input + add memo

--- src/shell/script.rs
+++ src/shell/script.rs
@@ -8,6 +8,8 @@ pub enum Token<'a> {
     Dollar,
 }
 
+// TODO: Idea: pass Tokenizer as Arguments to each command handler as an iterator
+//             and if EOF or ';' is encountered, return None, signifying the end of input
 pub struct TokenizedIterator<'a> {
     input: &'a str,
     index: usize,
@@ -27,12 +29,16 @@ impl<'a> TokenizedIterator<'a> {
         self.input.as_bytes().get(self.index).map(|v| *v)
     }
 
-    fn is_whitespace(&self) -> bool {
-        matches!(self.current(), Some(b' ')) || matches!(self.current(), Some(b'\t'))
+    fn is_eof(&self) -> bool {
+        self.index >= self.input.len()
     }
 
-    fn eof(&self) -> bool {
-        self.index >= self.input.len()
+    fn is_whitespace(&self) -> bool {
+        matches!(self.current(), Some(b' ') | Some(b'\t'))
+    }
+
+    fn is_special(&self) -> bool {
+        matches!(self.current(), Some(b';') | Some(b'$'))
     }
 }
 
@@ -40,20 +46,19 @@ impl<'a> Iterator for TokenizedIterator<'a> {
     type Item = Token<'a>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.eof() {
+        if self.is_eof() {
             return None;
         }
 
         while self.is_whitespace() {
             self.index += 1;
 
-            if self.eof() {
+            if self.is_eof() {
                 return None;
             }
         }
 
         if matches!(self.current(), Some(b';')) {
-
             self.index += 1;
             return Some(Token::Semicolon);
         }
@@ -65,7 +70,7 @@ impl<'a> Iterator for TokenizedIterator<'a> {
 
         self.start = self.index;
 
-        while !self.is_whitespace() && !self.eof() {
+        while !self.is_eof() && !self.is_whitespace() && !self.is_special() {
             self.index += 1;
         }
 
@@ -119,7 +124,7 @@ impl Environment {
     pub fn run(&self, src: &str) {
         let mut tokens = TokenizedIterator::new(src);
 
-        while !tokens.eof() {
+        while !tokens.is_eof() {
             let mut args = Arguments::new();
 
             while let Some(token) = tokens.next() {
